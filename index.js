e17b3be6e1cde64e8eb18c27614082a892c6e446
yogev malca <hudchudchudc@gmail.com>
2019-06-28 18:34:48 +0300

    Added test code in comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 
 module.exports = (_filesData) => {
+
   try {
     initializeProcess(_filesData);
   } catch (error) {
@@ -9,28 +10,29 @@ module.exports = (_filesData) => {
   }
 };
 
-const initializeProcess = filesData => {
+const initializeProcess = (filesData) => {
   Object.keys(filesData).forEach(sourceFolderPath => {
       if (filesData[sourceFolderPath].allFiles) {
         //Get all files in folder and add it to the object as files
-      }
+      };
 
       if (filesData[sourceFolderPath].fullyChosen) {
         //Copy all the directory(include diractories)        
         copyFolder(sourceFolderPath, filesData[sourceFolderPath].destination);
         return;
-      }
-      
+      };
+
       copyFiles(filesData[sourceFolderPath], sourceFolderPath);
+
     }
-  )
-}
+
+  );
+};
 
 const copyFolder = (sourceDirPath, destinationDirPath) => {
-  //CANT REACH THE SOURCE DIRECTORY NEED TO CHECK WHY
-  fs.copy(sourceDirPath, destinationDirPath, (err) => {
-    if (err) console.log(err)
-  })
+  fs.copySync(sourceDirPath, destinationDirPath, (err) => {
+    if (err) console.log("Err: " + err);
+  });
 };
 
 const copyFiles = ({
@@ -41,3 +43,24 @@ const copyFiles = ({
     fs.copySync(`${sourceDirPath}/${file}`, `${destination}/${file}`)
   );
 };
+
+// // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const filesData = {
+//   'c:\\yogev\\Projects\\source-dir-test': {
+//     files: ["textfile.txt"],
+//     destination: "c:\\yogev\\Projects\\destination-dir-test"
+//   },
+//   'c:\\yogev\\Projects\\source-dir-test\\test1': {
+//     files: ["textfiletest1.txt"],
+//     destination: "c:\\yogev\\Projects\\destination-dir-test\\test1\\inner"
+//   },
+//   "c:\\yogev\\Projects\\source-dir-test\\test2": {
+//     fullyChosen: true,
+//     destination: "c:\\yogev\\Projects\\destination-dir-test\\test2"
+//   },
+// }
+
+
+// initializeProcess(filesData);
+
+// //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
