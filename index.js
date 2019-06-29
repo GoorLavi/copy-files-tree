37c34f676633d23cf2e7f89d14c3bffb5adda3a1
Goor <goor.lavi@plainid.com>
2019-06-30 01:16:31 +0300

    done

--- index.js
+++ index.js
@@ -1,66 +1,45 @@
 const fs = require('fs-extra');
 
-module.exports = (_filesData) => {
 
-  try {
-    initializeProcess(_filesData);
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+const getAllFilteredItems = (path, isAllDirectory, isAllFiles) => {
+
+    return (fs.readdirSync(path) || []).filter(item => {
+        const isDirectory = fs.statSync(`${path}/${item}`).isDirectory();
+        return (isDirectory && isAllDirectory || !isDirectory && isAllFiles);
+    });
 };
 
-const initializeProcess = (filesData) => {
-  Object.keys(filesData).forEach(sourceFolderPath => {
-      if (filesData[sourceFolderPath].allFiles) {
-        //Get all files in folder and add it to the object as files
-      };
+const copyItems = ({items, destination, sourceDirPath}) =>
+    items.forEach(file =>
+        fs.copySync(`${sourceDirPath}/${file}`, `${destination}/${file}`)
+    );
 
-      if (filesData[sourceFolderPath].fullyChosen) {
-        //Copy all the directory(include diractories)        
-        copyFolder(sourceFolderPath, filesData[sourceFolderPath].destination);
-        return;
-      };
+const copyFiles = async filesData => {
+};
 
-      copyFiles(filesData[sourceFolderPath], sourceFolderPath);
 
+const copyFilesSync = filesData => {
+
+    try {
+        Object.keys(filesData).forEach(sourceDirPath => {
+
+                const folderObject = filesData[sourceDirPath];
+                const {options: {allFiles, allDirectories} = {}, destination, foldersAndFiles = []} = folderObject;
+
+                const items = [...foldersAndFiles, ...getAllFilteredItems(sourceDirPath, allDirectories, allFiles)];
+
+                copyItems({items, destination, sourceDirPath});
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        return error;
     }
-
-  );
 };
 
-const copyFolder = (sourceDirPath, destinationDirPath) => {
-  fs.copySync(sourceDirPath, destinationDirPath, (err) => {
-    if (err) console.log("Err: " + err);
-  });
+module.exports = {
+    copyFilesSync,
+    copyFiles,
 };
 
-const copyFiles = ({
-  files,
-  destination
-}, sourceDirPath) => {
-  files.forEach(file =>
-    fs.copySync(`${sourceDirPath}/${file}`, `${destination}/${file}`)
-  );
-};
 
-// // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// const filesData = {
-//   'c:\\yogev\\Projects\\source-dir-test': {
-//     files: ["textfile.txt"],
-//     destination: "c:\\yogev\\Projects\\destination-dir-test"
-//   },
-//   'c:\\yogev\\Projects\\source-dir-test\\test1': {
-//     files: ["textfiletest1.txt"],
-//     destination: "c:\\yogev\\Projects\\destination-dir-test\\test1\\inner"
-//   },
-//   "c:\\yogev\\Projects\\source-dir-test\\test2": {
-//     fullyChosen: true,
-//     destination: "c:\\yogev\\Projects\\destination-dir-test\\test2"
-//   },
-// }
-
-
-// initializeProcess(filesData);
-
-// //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
