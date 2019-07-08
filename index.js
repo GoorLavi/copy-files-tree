0782e8548c97cd0bd44f38ea31c098c8c1730b79
Goor <goor.lavi@plainid.com>
2019-07-08 20:18:07 +0300

    need to do last check

--- index.js
+++ index.js
@@ -2,98 +2,85 @@ const fs = require('fs-extra');
 
 const getAllFilteredItems = (path, isAllDirectory, isAllFiles) => {
 
-  return (fs.readdirSync(path) || []).filter(item => {
-    const isDirectory = fs.statSync(`${path}/${item}`).isDirectory();
-    return (isDirectory && isAllDirectory || !isDirectory && isAllFiles);
-  });
+    return (fs.readdirSync(path) || []).filter(item => {
+        const isDirectory = fs.statSync(`${path}/${item}`).isDirectory();
+        return (isDirectory && isAllDirectory || !isDirectory && isAllFiles);
+    });
 };
 
 const copyItemsSync = ({
-    items,
-    destination,
-    sourceDirPath
-  }) =>
-  items.forEach(item =>
-    fs.copySync(`${sourceDirPath}/${item}`, `${destination}/${item}`)
-  );
+                           items,
+                           destination,
+                           sourceDirPath
+                       }) =>
+    items.forEach(item =>
+        fs.copySync(`${sourceDirPath}/${item}`, `${destination}/${item}`)
+    );
 
 const copyFilesSync = filesData => {
 
-  try {
-    for (sourceDirPath in filesData) {
-      const folderObject = filesData[sourceDirPath];
-      const {
-        options: {
-          allFiles,
-          allDirectories
-        } = {},
-        destination,
-        foldersAndFiles = []
-      } = folderObject;
+    try {
+        for (let sourceDirPath in filesData) {
+            const {
+                options: {
+                    allFiles,
+                    allDirectories
+                } = {},
+                destination,
+                foldersAndFiles = []
+            } = filesData[sourceDirPath];
 
-      const items = [...foldersAndFiles, ...getAllFilteredItems(sourceDirPath, allDirectories, allFiles)];
+            const filteredItems = getAllFilteredItems(sourceDirPath, allDirectories, allFiles);
+            const items = foldersAndFiles.concat(filteredItems);
+            copyItemsSync({items, destination, sourceDirPath});
+        }
 
-      copyItemsSync({
-        items,
-        destination,
-        sourceDirPath
-      });
+    } catch (error) {
+        console.error(error);
+        return error;
     }
-
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
 };
 
-const copyItems = ({
-    items,
-    destination,
-    sourceDirPath
-  }) =>
-  items.map(item =>
-    new Promise((resolve, reject) => {
-      fs.copy(`${sourceDirPath}/${item}`, `${destination}/${item}`, err => {
-        if (err)
-          reject(err);
-        resolve();
-      });
-    })
-  );
+const copyItems = ({items, destination, sourceDirPath}) =>
+    items.map(item =>
+        new Promise((resolve, reject) => {
+            fs.copy(`${sourceDirPath}/${item}`, `${destination}/${item}`, err => {
+                if (err)
+                    reject(err);
+                resolve();
+            });
+        })
+    );
 
 
 const copyFiles = async filesData => {
 
-  try {
-    let allPromises = [];
+    try {
+        let allPromises = [];
 
-    for (sourceDirPath in filesData) {
-      const folderObject = filesData[sourceDirPath];
-      const {
-        options: {
-          allFiles,
-          allDirectories
-        } = {},
-        destination,
-        foldersAndFiles = []
-      } = folderObject;
-      const items = [...foldersAndFiles, ...getAllFilteredItems(sourceDirPath, allDirectories, allFiles)];
+        for (let sourceDirPath in filesData) {
+            const {
+                options: {
+                    allFiles,
+                    allDirectories
+                } = {},
+                destination,
+                foldersAndFiles = []
+            } = filesData[sourceDirPath];
 
-      allPromises = [...allPromises, ...copyItems({
-        items,
-        destination,
-        sourceDirPath
-      })];
+            const filteredItems = getAllFilteredItems(sourceDirPath, allDirectories, allFiles);
+            const items = foldersAndFiles.concat(filteredItems);
+            allPromises = allPromises.concat(copyItems({items, destination, sourceDirPath}));
+        }
+
+        await Promise.all(allPromises);
+    } catch (error) {
+        return error;
     }
-
-    await Promise.all(allPromises);
-  } catch (error) {
-    return error;
-  }
 
 }
 
 module.exports = {
-  copyFilesSync,
-  copyFiles
+    copyFilesSync,
+    copyFiles
 };
